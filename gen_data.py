62648706e4de86d9e069c1342cbac30f9d9934df
Archit Kashyap <archit.kashyap@iiitb.org>
2019-10-19 17:01:17 +0530

    Code for Test and Train data genertion

--- gen_data.py
+++ gen_data.py
@@ -28,29 +28,37 @@ def get_nm():
     del data
     return lt_nm
 
-def get_test_data():
+def get_train_data():
     '''
-    Generate a list of all the test images in CIFAR-10
+    Generate a list of all the train images in CIFAR-10
     '''
-    lt_img = np.empty([0])
-    lt_lab = []
+    lt_img_train = np.empty([0])
+    lt_lab_train = []
     path = 'cifar-10/data_batch_'
     for i in range(5):
         data = unpickle(path+str(i+1))
         tmp_img = data[b'data']
         tmp_lab = data[b'labels']
-        lt_lab.extend(tmp_lab)
-        lt_img = np.append(lt_img, tmp_img)
+        lt_lab_train.extend(tmp_lab)
+        lt_img_train = np.append(lt_img_train, tmp_img)
         del data, tmp_img, tmp_lab
 
-    lt_img = get_img(50000, lt_img)
-    lt_img = lt_img.astype(int)
-    return lt_img, lt_lab
+    lt_img_train = get_img(50000, lt_img_train)
+    lt_img_train = lt_img_train.astype(int)
+    return lt_img_train, lt_lab_train
 
+def get_test_data():
+    '''
+    Generate a list of all the test images in CIFAR-10
+    '''
+    data = unpickle('cifar-10/test_batch')
+    lt_img_test = data[b'data']
+    lt_lab_test = data[b'labels']
 
-#%%
-lt_nm = get_nm()
-lt_img, lt_lab = get_test_data()
+    lt_img_test = get_img(10000, lt_img_test)
+    lt_img_test = lt_img_test.astype(int)
+    return lt_img_test, lt_lab_test
+
 
 #%%
 def img_resize(img, factor):
@@ -64,14 +72,13 @@ def img_resize(img, factor):
     res = cv2.resize(img, (w, w))
     return res.astype('int')
 
-#%%
-def get_random_img_idx():
+def get_random_img_idx(sz):
     '''
     Get a random list of image number, their scaling factor and their height 
     the number of images vary between 2 and 5
     '''
     n = random.randint(2, 5)
-    idx = random.sample(range(50000), n)
+    idx = random.sample(range(sz), n)
     fact = np.empty([0])
     ht = np.empty([0])
     for i in range(n):
@@ -80,7 +87,6 @@ def get_random_img_idx():
         ht = np.append(ht, int(32*x))
     return idx, fact, ht.astype('int') 
 
-#%%
 def is_overlap(x, y, ht):
     '''
     Given the x and y co-ordinates(mid-point) of the images and their height
@@ -127,11 +133,18 @@ def get_cor(ht):
         y = random.sample(range(0, 224-max_ht), n)
     return x, y
 
-#%%
-def gen_image(x,y,idx,fact):
+def gen_image(x,y,idx,fact,flag):
+    '''
+    Generate the resultant image given the random image list
+    '''
+    if flag==1:
+        data = lt_img_train
+    else:
+        data = lt_img_test
+
     img = np.zeros((224,224,3))
     for i in range(len(idx)):
-        tmp = img_resize(lt_img[idx[i]], fact[i])
+        tmp = img_resize(data[idx[i]], fact[i])
         l = tmp.shape[0]
         sx = x[i]
         sy = y[i]
@@ -142,7 +155,60 @@ def gen_image(x,y,idx,fact):
                 img[sx+cx][sy+cy] = tmp[cx][cy]
     return img.astype('int')
 
+
 #%%
-idx, fact, ht = get_random_img_idx()
-x, y = get_cor(ht)
-res = gen_image(x, y, idx, fact)
+def make_train(sz, num):
+    '''
+    Generate the test data
+    '''
+    file1 = open('data/train/meta.txt', 'a')
+    for i in range(sz):
+        # generate the image
+        idx, fact, ht = get_random_img_idx(50000)
+        x, y = get_cor(ht)
+        res = gen_image(x, y, idx, fact, 1)
+        nm = str(num+i+1)+'.jpg'
+        cv2.imwrite('data/train/'+nm, res)
+        # record the y values
+        n = len(idx)
+        file1.write(nm+' ')
+        for j in range(n):
+            l = int(ht[j]/2)
+            file1.write(str(x[j]+l)+' '+str(y[j]+l)+' '+str(ht[j])+' '+str(ht[j])+' '+str(lt_lab_train[idx[j]]))
+            if j==n-1:
+                file1.write('\n')
+            else:
+                file1.write(' ')
+    file1.close()
+    return 0
+
+#%%
+def make_test(sz, num):
+    '''
+    Generate the test data
+    '''
+    file1 = open('data/test/meta.txt', 'a')
+    for i in range(sz):
+        # generate the image
+        idx, fact, ht = get_random_img_idx(10000)
+        x, y = get_cor(ht)
+        res = gen_image(x, y, idx, fact, 0)
+        nm = str(num+i+1)+'.jpg'
+        cv2.imwrite('data/test/'+nm, res)
+        # record the y values
+        n = len(idx)
+        file1.write(nm+' ')
+        for j in range(n):
+            l = int(ht[j]/2)
+            file1.write(str(x[j]+l)+' '+str(y[j]+l)+' '+str(ht[j])+' '+str(ht[j])+' '+str(lt_lab_train[idx[j]]))
+            if j==n-1:
+                file1.write('\n')
+            else:
+                file1.write(' ')
+    file1.close()
+    return 0
+
+#%%
+lt_nm = get_nm()
+lt_img_train, lt_lab_train = get_train_data()
+lt_img_test, lt_lab_test = get_test_data()
